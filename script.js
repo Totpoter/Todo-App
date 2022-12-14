46292e63873bded43ebce3afbba197d8f04f8ba0
Totpoter <nsbdev@gmail.com>
2022-12-14 18:39:00 +0100

    Added modal functionality and some placeholders

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const logo = document.querySelector(".logo");
+const todoTitle = document.querySelector(".todoTitle");
+const todoText = document.querySelector(".todoText");
+const modal = document.querySelector(".modalAdd");
 
 logo.addEventListener("click", () => {
     window.location.href="index.html";
-})
+});
+
+/* Open Todo Modal */
+
+const openModal = () => {
+    modal.classList.toggle("openModal");
+};
+
+/* Add Text & Title */
+
+/* Update page with new Todo & Close modal */
+
+/* Mark Todo as complete /*
+
+
+
+/* Delete Todo */
+
+// Confirm Delete (modal)
+
+// Delete Todo from page
+
+// Close modal
+
